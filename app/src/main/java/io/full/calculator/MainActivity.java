908dbfcd709b5ae61895ea50a60fdcba1510260d
Shangeeth Sivan <Shangeeth.Sivan@adaptavancloud.com>
2017-04-06 11:57:06 +0530

    Clearing issues Part 1

--- app/src/main/java/io/full/calculator/MainActivity.java
+++ app/src/main/java/io/full/calculator/MainActivity.java
@@ -3,6 +3,7 @@ package io.full.calculator;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,7 +17,9 @@ public class MainActivity extends AppCompatActivity {
     TextView operatorLine;
     TextView mainLine;
 
-    operatorsEnum currentOperator ;
+    operatorsEnum currentOperator;
+
+    boolean decimalAdded = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +27,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        init();
+    }
+
+    private void init(){
+
         backLine = (TextView) findViewById(R.id.backLine);
         operatorLine = (TextView) findViewById(R.id.operator);
         mainLine = (TextView) findViewById(R.id.mainLine);
 
     }
 
-    public void addNumber(String number) {
+    public void addNumberToMainLine(String number) {
 
         mainLine.setText(mainLine.getText() + number);
 
@@ -40,20 +48,34 @@ public class MainActivity extends AppCompatActivity {
 
         backLine.setText("");
         operatorLine.setText("");
-        mainLine.setText("");
+        mainLine.setText(getResources().getText(R.string.default_value));
+
+        decimalAdded = false;
 
     }
 
     public void deleteClicked() {
 
-        if (mainLine.length() != 0)
+        if (mainLine.length() != 0) {
+
+            if (mainLine.getText().toString().substring(mainLine.length() - 1).equals(".")) {
+                decimalAdded = false;
+            }
+
             mainLine.setText(mainLine.getText().toString().substring(0, mainLine.length() - 1));
+        }
+
+        if (mainLine.getText().length() == 0) {
+            mainLine.setText(getResources().getText(R.string.default_value));
+        }
+
 
     }
 
     public void operatorClicked(operatorsEnum operator) {
 
         String operationCharacter = "";
+
         switch (operator) {
             case ADD:
                 operationCharacter = "+";
@@ -78,9 +100,14 @@ public class MainActivity extends AppCompatActivity {
         }
 
         operatorLine.setText(operationCharacter);
+        String a = mainLine.getText().toString();
 
-        if(backLine.getText().length() == 0)
-            backLine.setText(mainLine.getText());
+        Log.e("LOGGER", "operatorClicked: "+a );
+        if (backLine.getText().length() == 0) {
+            backLine.setText(mainLine.getText().toString());
+            mainLine.setText(getResources().getText(R.string.default_value));
+        }
+        //TODO: Add zero to back stack
 
     }
 
@@ -89,71 +116,81 @@ public class MainActivity extends AppCompatActivity {
 
         int id = view.getId();
 
-        if(mainLine.getText().equals("0") && mainLine.getText().length() == 1)
+        if (mainLine.getText().equals("0") && mainLine.getText().length() == 1 && id != R.id.decimalPoint)
             mainLine.setText("");
 
         switch (id) {
 
             case R.id.number_0:
                 if (mainLine.getText().length() != 1 && !mainLine.getText().equals("0"))
-                    addNumber("0");
+                    addNumberToMainLine("0");
                 break;
 
             case R.id.number_1:
-                addNumber("1");
+                addNumberToMainLine("1");
                 break;
 
             case R.id.number_2:
-                addNumber("2");
+                addNumberToMainLine("2");
                 break;
 
             case R.id.number_3:
-                addNumber("3");
+                addNumberToMainLine("3");
                 break;
 
             case R.id.number_4:
-                addNumber("4");
+                addNumberToMainLine("4");
                 break;
 
             case R.id.number_5:
-                addNumber("5");
+                addNumberToMainLine("5");
                 break;
 
             case R.id.number_6:
-                addNumber("6");
+                addNumberToMainLine("6");
                 break;
 
             case R.id.number_7:
-                addNumber("7");
+                addNumberToMainLine("7");
                 break;
 
             case R.id.number_8:
-                addNumber("8");
+                addNumberToMainLine("8");
                 break;
 
             case R.id.number_9:
-                addNumber("9");
+                addNumberToMainLine("9");
                 break;
 
             case R.id.decimalPoint:
-                addNumber(".");
+
+                if (!decimalAdded) {
+                    addNumberToMainLine(".");
+                    decimalAdded = true;
+                }
+
                 break;
 
             case R.id.operation_add:
                 operatorClicked(operatorsEnum.ADD);
                 break;
-            case R.id.operation_subtract:
 
+            case R.id.operation_subtract:
                 operatorClicked(operatorsEnum.SUBTRACT);
                 break;
-            case R.id.operation_multiply:
 
+            case R.id.operation_multiply:
                 operatorClicked(operatorsEnum.MULTIPLY);
                 break;
-            case R.id.operation_modulo:
 
+            case R.id.operation_division:
+                operatorClicked(operatorsEnum.DIVISION);
+                break;
+
+            case R.id.operation_modulo:
                 operatorClicked(operatorsEnum.MODULO);
                 break;
+
             case R.id.operation_delete:
                 deleteClicked();
                 break;
